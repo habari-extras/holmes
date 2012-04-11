0f3241175b7e090bab0da4b413f75e516df7ad52
Morgante Pell <morgante.pell@morgante.net>
2012-04-11 12:43:03 -0600

    add basic rules

--- holmes.plugin.php
+++ holmes.plugin.php
@@ -1,6 +1,26 @@
 <?php
 class Holmes extends Plugin
 {
+	
+	/**
+	 * Simplistic solution to just provide a bunch of scopes
+	 */
+	public function filter_get_scopes($scopes)
+		{
+			$rules = RewriteRules::get_active();
+			$scopeid = 40000;
+			foreach($rules as $rule) {
+				$scope = new StdClass();
+				$scope->id = $scopeid++;
+				$scope->name = $rule->name;
+				$scope->priority = 15;
+				$scope->criteria = array(
+					array('request', $rule->name),
+				);
+				$scopes[] = $scope;
+			}
 
+			return $scopes;
+		}
 }
 ?>
